a57f9e1cc31a73445cad5577db2ed2451cb47f44
Sherry Ji <jiqianlan@gmail.com>
2025-09-22 16:42:04 -0500

    Update 09. Data Segmentation.sql

--- MYSQL project/Scripts/09. Data Segmentation.sql
+++ MYSQL project/Scripts/09. Data Segmentation.sql
@@ -15,7 +15,7 @@ SELECT
 product_key,
 product_name,
 cost,
-CASE WHEN cost < 100 THEN 'Belwo 100'
+CASE WHEN cost < 100 THEN 'Below 100'
      WHEN cost BETWEEN 100 AND 500 THEN '100-500'
      WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
      ELSE 'ABOVE 1000'
