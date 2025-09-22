c372ba57b1b64a38d50eca33e19059ecfed56bf1
Sherry Ji <jiqianlan@gmail.com>
2025-09-22 16:44:35 -0500

    Update 09. Data Segmentation.sql

--- MYSQL project/Scripts/09. Data Segmentation.sql
+++ MYSQL project/Scripts/09. Data Segmentation.sql
@@ -18,7 +18,7 @@ cost,
 CASE WHEN cost < 100 THEN 'Below 100'
      WHEN cost BETWEEN 100 AND 500 THEN '100-500'
      WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
-     ELSE 'ABOVE 1000'
+     ELSE 'Above 1000'
 END cost_range
 FROM gold_dim_products)
 
